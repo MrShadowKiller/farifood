7532dae79cc44f38a7abb4a562061a12d48384cf
MrShadowKiller <m.jooker2016@gmail.com>
2021-05-10 14:07:35 +0430

    fix: change FoodRate class name to UserRate

--- src/main/java/ir/ac/kntu/UserRate.java
+++ src/main/java/ir/ac/kntu/UserRate.java
@@ -1,11 +1,11 @@
 package ir.ac.kntu;
 
-public enum FoodRate {
+public enum UserRate {
     VERY_BAD(1), BAD(2), AVERAGE(3), GOOD(4), GREAT(5);
 
     private int rate;
 
-    FoodRate(int rate) {
+    UserRate(int rate) {
         this.rate = rate;
     }
 
