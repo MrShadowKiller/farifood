aeeae506ec7a3e98cf96206219744b8b17918abc
MrShadowKiller <m.jooker2016@gmail.com>
2021-05-11 13:39:10 +0430

    fix: change WeekSchedule name to WeekDays

--- src/main/java/ir/ac/kntu/WeekDays.java
+++ src/main/java/ir/ac/kntu/WeekDays.java
@@ -1,15 +1,16 @@
 package ir.ac.kntu;
 
-public enum WeeklySchedule {
+public enum WeekDays {
     SATURDAY(0),SUNDAY(1),MONDAY(2),TUESDAY(3),WEDNESDAY(4),
     THURSDAY(5),FRIDAY(6);
 
     private int rate;
 
-    private int availability;
+    private boolean availability;
 
-    WeeklySchedule(int rate) {
+    WeekDays(int rate) {
         this.rate = rate;
+        availability = false;
     }
 
     public int getRate() {
@@ -20,11 +21,11 @@ public enum WeeklySchedule {
         this.rate = rate;
     }
 
-    public int getAvailability() {
+    public boolean getAvailability() {
         return availability;
     }
 
-    public void setAvailability(int availability) {
+    public void setAvailability(boolean availability) {
         this.availability = availability;
     }
 }
