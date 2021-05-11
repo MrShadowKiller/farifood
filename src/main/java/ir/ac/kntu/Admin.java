da1c4a60749f168e491b76ecb907d831604c9e64
MrShadowKiller <m.jooker2016@gmail.com>
2021-05-11 18:32:54 +0430

    feat: Implement Admin Class

---
+++ src/main/java/ir/ac/kntu/Admin.java
@@ -0,0 +1,11 @@
+package ir.ac.kntu;
+
+public class Admin extends Customer{
+
+    public Admin(String firstName, String lastName, String phoneNumber,
+                 String username, String password, String address) {
+        super(firstName, lastName, phoneNumber, username, password, address);
+    }
+
+
+}
