f76fbf0ece11f026add2e812431b6b7c6107e265
MrShadowKiller <m.jooker2016@gmail.com>
2021-05-13 19:25:40 +0430

    feat: add Order Status and Schedule

---
+++ src/main/java/ir/ac/kntu/DeliverySchedule.java
@@ -0,0 +1,31 @@
+package ir.ac.kntu;
+
+public enum DeliverySchedule {
+    SATURDAY(0),SUNDAY(1),MONDAY(2),TUESDAY(3),WEDNESDAY(4),
+    THURSDAY(5),FRIDAY(6);
+
+    private int rate;
+
+    private int restaurantIndex;
+
+    DeliverySchedule(int rate) {
+        this.rate = rate;
+        restaurantIndex = -1;
+    }
+
+    public int getRate() {
+        return rate;
+    }
+
+    public void setRate(int rate) {
+        this.rate = rate;
+    }
+
+    public int getRestaurantIndex() {
+        return restaurantIndex;
+    }
+
+    public void setRestaurantIndex(int restaurantIndex) {
+        this.restaurantIndex= restaurantIndex;
+    }
+}
