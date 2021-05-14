855cda61f50fc2aa83db6d77a377b0fb44525104
MrShadowKiller <m.jooker2016@gmail.com>
2021-05-14 18:20:22 +0430

    feat: add ScannerWrapper

---
+++ src/main/java/ir/ac/kntu/ScannerWrapper.java
@@ -0,0 +1,30 @@
+package ir.ac.kntu;
+
+import java.util.Scanner;
+
+public class ScannerWrapper {
+    private static ScannerWrapper instance = new ScannerWrapper();
+    private Scanner scanner;
+
+    public static ScannerWrapper getInstance(){
+        return instance;
+    }
+
+    private ScannerWrapper(){
+        scanner = new Scanner(System.in);
+    }
+
+    public int nextInt(){
+        int n = scanner.nextInt();
+        scanner.skip("\n");
+        return n;
+    }
+
+    public double nextDouble(){
+        return scanner.nextDouble();
+    }
+
+    public String nextLine(){
+        return scanner.nextLine();
+    }
+}
